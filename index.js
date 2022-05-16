9b25238fdbf635c38d1d4ea079b1feae51b9e147
YESENIA2012 <gyesenia173@gmail.com>
2022-05-16 16:01:29 -0500

    Added eventListener to change opacity of id createHomework element

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ const modal = document.getElementById("modalContainer");
 openModal.addEventListener("click", () => {
   modalContainer.classList.add("show");
 });
+
+const openHomework = document.getElementById("openHomework");
+const containerHomework = document.getElementById("createHomework");
+const body = document.querySelector("body");
+
+openHomework.addEventListener("click", () => {
+  createHomework.classList.add("show2");
+  modalContainer.classList.remove("show");
+  body.style.backgroundColor = "transparent";
+});
+
+const closeButton = document.getElementById("closeButton");
+
+closeButton.addEventListener("click", () => {
+  createHomework.classList.remove("show2");
+});
